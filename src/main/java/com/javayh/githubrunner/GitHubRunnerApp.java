92bd998128a3bfc5756d31df9827b0497ccc859d
yanghaiji <hai.ji.yang@ibm.com>
2023-02-13 09:47:11 +0800

    Initial commit

---
+++ src/main/java/com/javayh/githubrunner/GitHubRunnerApp.java
@@ -0,0 +1,20 @@
+package com.javayh.githubrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author hai ji
+ * @version 1.0.0
+ * @since 2023-02-13
+ */
+@SpringBootApplication
+public class GitHubRunnerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubRunnerApp.class, args);
+    }
+}
